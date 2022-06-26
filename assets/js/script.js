e8c457194600d7dd88b217228a714a1355a4f0d2
fghoneycutt <fghoneycutt@gmail.com>
2022-06-25 20:09:50 -0400

    added ability to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,6 @@ var taskFormHandler = function(event){
 }
 
 var createTaskEl = function(taskDataObj){
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -61,6 +59,8 @@ var createTaskEl = function(taskDataObj){
    
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
+
+    saveTasks();
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
@@ -140,6 +140,7 @@ var deleteTask = function(taskId){
     }
     //reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 };
 var editTask = function(taskId){
     //get task list item element
@@ -166,6 +167,8 @@ var completeEditTask = function(taskName, taskType, taskId){
             tasks[i].type = taskType;
         }
     }
+
+    saveTasks();
 
     alert("Task Updated!");
 
@@ -196,8 +199,11 @@ var taskStatusChangeHandler = function(event){
         if (tasks[i].id === parseInt(taskId)){
             tasks[i].status = statusValue;
         }
-        console.log(tasks);
     }
+    saveTasks();
+}
+var saveTasks = function(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 pageContentEl.addEventListener("click", taskButtonHandler)
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
